da842709b08400249d705ec10cb88d3c054ba151
AnastasiaG <anastasiya_galaxy@mail.ru>
2022-02-24 22:45:28 +0300

    task solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  const positiveNum = Math.abs(n).toString();
+  const arr = [];
+  const reversedArr = [];
+  for (let i = 0; i < positiveNum.length; i++) {
+    arr[i] = positiveNum.charAt(i);
+    reversedArr.unshift(arr[i]);
+  }
+  return +reversedArr.join('')
 }
